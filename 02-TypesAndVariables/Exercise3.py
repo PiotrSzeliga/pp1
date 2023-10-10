f616e9d14918abe3588102902a9f638e328efd52
Piotr Szeliga <piotrszeliga87@gmail.com>
2023-10-10 19:07:55 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/Exercise3.py
@@ -0,0 +1,4 @@
+hours=int(input("Enter hours: "))
+rate=int(input("Enter hourly rate: "))
+pay=hours*rate
+print("Pay:",pay)
---
+++ 02-TypesAndVariables/Exercise5.py
@@ -0,0 +1,3 @@
+temp1=float(input("Input temperature in Celsius to convert:"))
+temp2=temp1*1.8+32
+print("Temperature in Farenheit: ",temp2)
