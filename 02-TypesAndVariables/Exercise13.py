e737f70aeacfd121e4ef3dc7eeb067dd1c91e4dd
s-013-32 <s-013-32@gumiforest.ae.krakow.pl>
2023-10-11 13:02:43 +0200

    essa

---
+++ 02-TypesAndVariables/Exercise13.py
@@ -0,0 +1,3 @@
+cube_side = int(input("Enter cube side: "))
+cube_surface_area = cube_side**2*6
+print(F"The surface area of a cube with side {cube_side} is {cube_surface_area}")
---
+++ 02-TypesAndVariables/Exercise9.py
@@ -0,0 +1,9 @@
+n1 = 5
+n2 = 1
+n3 = 8
+n4 = 6
+n5 = 3
+print("sum of all variables: ",n1+n2+n3+n4+n5,)
+print("sum of squared variables: ",n1**2+n2**2+n3**2+n4**2+ n5**2)
+print("quotient of the variable three and five:" , n3/n5)
+print("message (True / False) indicating if the third variable is equal to the fourth variable" , bool(n3=n4))
