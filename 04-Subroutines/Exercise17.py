5e7b3dc6bc3a28265032a46d4073d74c04325748
s-013-32 <s-013-32@gumiforest.ae.krakow.pl>
2023-10-25 13:01:09 +0200

    elko

---
+++ 04-Subroutines/Exercise17.py
@@ -0,0 +1,13 @@
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+n3=int(input("Enter third number: "))
+
+def different(n1,n2,n3): 
+    if n1 != n2 != n3:
+        return True
+    else:
+        return False
+    
+if different(n1,n2,n3):
+    print(f"Numbers {n1}, {n2}, and {n3} are different")
+    
